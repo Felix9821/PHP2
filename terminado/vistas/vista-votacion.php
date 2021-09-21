f02d4ebaecbacae1d24d5c6b2edc02cdcb8f485b
Felix9821 <82671041+Felix9821@users.noreply.github.com>
2021-09-21 02:59:51 -0500

    Add files via upload

---
+++ terminado/vistas/vista-votacion.php
@@ -0,0 +1,7 @@
+<input type="radio" name="encuesta satisfaccion" id="" value="muy satisfecho">muy satisfacecho<br>
+<input type="radio" name="encuesta satisfaccion" id="" value="satisfacecho">satisfecho<br>
+<input type="radio" name="encuesta satisfaccion" id="" value="neutro">neutro<br>
+<input type="radio" name="encuesta satisfaccion" id="" value="insatisfecho">insatisfecho<br>
+<input type="radio" name="encuesta satisfaccion" id="" value="muy insatisfecho">muy insatisfecho<br>
+
+<input type="submit" value="Votar!">
